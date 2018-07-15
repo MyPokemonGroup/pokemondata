50e243b40feafa5f65a698387685b5eca2d5f396
Alvin Kang <oneinallin1@gmail.com>
2018-07-14 21:22:49 -0700

    Implement proper error-handling wrapper async fetch function

---
+++ utils/fetch.js
@@ -0,0 +1,25 @@
+/**
+ * Asynchronous helper function to call fetch()
+ * *** USED FOR callFetch() ***
+ * @param {string} url
+ * @returns {json | error} json if fetch() is successful, error otherwise
+ */
+export async function asyncCall(url) {
+  let response = await fetch(url);
+  if (response.ok) return await response.json();
+  throw new Error(response.statusText);
+}
+
+/**
+ * Asynchronous wrapper over fetch() that properly handles errors
+ * @param {string} url
+ * @returns {json | error} json if fetch() is successful, error otherwise
+ */
+export async function callFetch(url) {
+  try {
+    return await asyncCall(url);
+  } catch (err) {
+    console.error("error", err);
+    throw new Error(err);
+  }
+}
