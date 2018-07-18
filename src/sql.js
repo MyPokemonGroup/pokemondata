4c1f78122f5ecc4b6a3df5ece574b9d36dc7bf54
Alvin Kang <oneinallin1@gmail.com>
2018-07-18 16:07:15 -0700

    Reformat

--- src/sql.js
+++ src/sql.js
@@ -1,14 +1,14 @@
-import Sequelize from 'sequelize';
-import * as config from './config.json';
+import Sequelize from "sequelize";
+import * as config from "./config.json";
 
 let sql = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
-    {
-        host: config.development.host,
-        dialect: config.development.dialect
-    }
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect
+  }
 );
 
 export default sql;
