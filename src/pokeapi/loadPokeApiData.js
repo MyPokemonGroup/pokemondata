dc83a64ef9b5dda4477c78792c773481c9c68d6e
Charles Weng <cweng8849@gmail.com>
2018-07-18 00:17:34 -0400

    adding hack for getting missing values from api calls

--- src/pokeapi/loadPokeApiData.js
+++ src/pokeapi/loadPokeApiData.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import PokeApiData from './PokeApiData.js';
+import sql from '../sql.js';
+import Sequelize from 'sequelize';
 
 const getPokeApiCount = () => {
     return axios.get('https://pokeapi.co/api/v2/pokemon-species/')
@@ -9,6 +11,13 @@ const getPokeApiCount = () => {
         .catch((error) => {
             return error;
         });
+};
+
+const matchPokeApiDataCount = (count) => {
+    return PokeApiData.count().then(c => {
+        console.log(count === c);
+        return count === c;
+    });
 };
 
 const isValidProperty = (obj, prop) => {
@@ -41,7 +50,7 @@ const getPokeApiData = (pokeNumber) => {
 };
 
 const setPokeApiData = (data) => {
-    if (!data) {
+    if (!data || !data.number || !data.description) {
         throw new Error('Unable to set null data');
     }
     let { number, description } = data;
@@ -51,10 +60,15 @@ const setPokeApiData = (data) => {
     });
 };
 
-const getAndSetPokeApiData = (pokeNumber) => {
+const getAndSetPokeApiData = (pokeNumber, backOff = 1) => {
     return getPokeApiData(pokeNumber)
         .then(setPokeApiData)
-        .catch((error) => getAndSetPokeApiData(pokeNumber));
+        .catch(() => {
+            setTimeout(() => {
+                getAndSetPokeApiData(pokeNumber, backOff * 2)}
+                , backOff * 5000
+            );
+        });
 };
 
 const loadPokeApiData = () => {
@@ -68,6 +82,52 @@ const loadPokeApiData = () => {
                 }
             )
     });
+
+    loadRemainingPokeApiData();
 };
 
-export default loadPokeApiData();
+
+const loadRemainingPokeApiData = (done = false) => {
+    if (done) {
+        return;
+    }
+    let selectMissingData = "SELECT\n" +
+        " CONCAT(z.expected, IF(z.got-1>z.expected, CONCAT(' ',z.got-1), '')) AS missing\n" +
+        "FROM (\n" +
+        " SELECT\n" +
+        "  @rownum:=@rownum+1 AS expected,\n" +
+        "  IF(@rownum=number, 0, @rownum:=number) AS got\n" +
+        " FROM\n" +
+        "  (SELECT @rownum:=0) AS a\n" +
+        "  JOIN pokeapidata\n" +
+        "  ORDER BY number\n" +
+        " ) AS z\n" +
+        "WHERE z.got!=0;";
+    getPokeApiCount()
+        .then(matchPokeApiDataCount).then((finished) => {
+            if (!finished) {
+                sql.query(selectMissingData, { type: Sequelize.QueryTypes.SELECT })
+                    .then((missed) => {
+                        console.log(missed);
+                        for (let { missing } of missed) {
+                            let range = missing.trim().split(' ');
+                            if (range) {
+                                if (range.length > 1) {
+                                    for (let i = parseInt(range[0]); i <= parseInt(range[1]); i++) {
+                                        getAndSetPokeApiData(i);
+                                    }
+                                } else {
+                                    getAndSetPokeApiData(parseInt(range[0]));
+                                }
+                            }
+                        }
+                    });
+            }
+            return loadRemainingPokeApiData(finished);
+        })
+        .catch(() => {
+            return loadRemainingPokeApiData();
+        });
+};
+
+export default loadPokeApiData;
