5bdf4fc7f55cc95781dd304b278476eaa098c4ca
Alvin Kang <oneinallin1@gmail.com>
2018-08-08 20:37:53 -0700

    Implement and run update script to update image URL to Amazon-s3 urls

---
+++ src/aws-s3/updateImagesTable.js
@@ -0,0 +1,28 @@
+import BastionApiData from '../bastionapi/BastionApiData';
+
+// Update the "image" an entry in the "images" table based on pokemonNumber
+function updateRowImage(pokemonNumber) {
+  // Format:
+  // https://s3-us-west-1.amazonaws.com/pokemon-images/images/30.png
+  const imageUrl = `https://s3-us-west-1.amazonaws.com/pokemon-images/images/${pokemonNumber}.png`;
+  const updateValue = { image: imageUrl };
+  BastionApiData.update(updateValue, {
+    where: { number: pokemonNumber }
+  }).then(affectedCount => {
+    console.log(`Successfully updated ${affectedCount} row(s)`);
+  });
+}
+
+// Update all images, up to maxPokemonNumber
+function updateImages(maxPokemonNumber = 807) {
+  for (
+    let pokemonNumber = 1;
+    pokemonNumber <= maxPokemonNumber;
+    pokemonNumber++
+  ) {
+    updateRowImage(pokemonNumber);
+  }
+}
+
+// Run to update images
+updateImages();
